53bbf982ce423e9b5d8b91fec49ce3e26674d428
Kieran <108032076+BritishCryptoGuy@users.noreply.github.com>
2023-08-04 21:51:22 +0100

    Started implementing modal code.

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,21 @@
 const buttonDiv = document.querySelector("#buttonDiv");
+const modalDiv = document.querySelector("#modalDiv");
+const modalFormDiv = document.querySelector("#modalFormDiv");
+const formQuestion = document.querySelector("#formQuestion");
+let workoutDataArray = [];
+let workoutObject = {
+  name: "empty",
+};
 
-buttonDiv.addEventListener("click", (e) => console.log(e));
+function clickMe(e) {
+  formQuestion.textContent = "What would you like to call this day/workout?";
+  modalDiv.classList.toggle("hidden");
+  buttonDiv.classList.toggle("hidden");
+}
+
+buttonDiv.addEventListener("click", (e) => clickMe(e));
+
+modalFormDiv.addEventListener("submit", (e) => {
+  e.preventDefault();
+  workoutObject.name = e.target.children[1].value;
+});
