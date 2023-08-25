5a8ba6fb8248a2ff59b9dd7b6d35caefba76d30b
Kieran <108032076+BritishCryptoGuy@users.noreply.github.com>
2023-08-25 11:04:10 +0100

    Started adding in question function loop

--- assets/index.js
+++ assets/index.js
@@ -2,6 +2,10 @@ const buttonDiv = document.querySelector("#buttonDiv");
 const modalDiv = document.querySelector("#modalDiv");
 const modalFormDiv = document.querySelector("#modalFormDiv");
 const formQuestion = document.querySelector("#formQuestion");
+const formInput = document.querySelector("#formInput");
+const buttonYes = document.querySelector("#formButtonYes");
+const buttonNo = document.querySelector("#formButtonNo");
+
 let workoutDataArray = [];
 let workoutObject = {
   name: "empty",
@@ -12,10 +16,19 @@ function clickMe(e) {
   modalDiv.classList.toggle("hidden");
   buttonDiv.classList.toggle("hidden");
 }
+function question() {
+  formQuestion.textContent = "Would you like to add an exercise?";
+  formInput.classList.toggle("hidden");
+  buttonNo.classList.toggle("hidden");
+  buttonYes.classList.toggle("hidden");
+}
 
 buttonDiv.addEventListener("click", (e) => clickMe(e));
 
 modalFormDiv.addEventListener("submit", (e) => {
   e.preventDefault();
+  console.log(e);
   workoutObject.name = e.target.children[1].value;
+  console.log(workoutObject);
+  question();
 });
